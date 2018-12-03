1a3ca3230ce0b95eecf108096b4fe637e1fbea24
Dmitriy Laushkin <mr.rufoos@gmail.com>
2018-12-03 15:43:26 +0300

    Init Rails 5.2.2 + VueJS

---
+++ README.md
@@ -0,0 +1,11 @@
+# Life Vitae
+
+This application uses Rails 5.2.2 + VueJS.
+
+* Ruby version: `2.1.5` with `RVM`
+
+* Node version: `9.11.2` with `nvm`
+
+* Package manager: `yarn`. For install just run: `npm i yarn -g`
+
+VueJS generated with [gem webpacker](https://github.com/rails/webpacker).
---
+++ app/javascript/components/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id='app'>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Life Vitae!"
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/app.scss';
+
+  $color-red: red;
+
+  p {
+    color: $color-red;
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
